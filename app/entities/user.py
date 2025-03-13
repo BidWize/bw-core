4ee8945f3881f130d701e4bf86a5906f0ad198c0
farestrad <80544970+farestrad@users.noreply.github.com>
2025-03-13 00:17:29 -0400

    Fares (#4)
    * feat: add user management functionality with CRUD operations and API endpoints
    
    * add delete/ update/ read
    
    * added delete/update/search
    
    * Added login functionality, more search, user info  and improved error messages
    
    * Added password hashing, login limiting, password restrictions
    
    ---------
    
    Co-authored-by: pwSathira <sathira.williams@gmail.com>

--- app/entities/user.py
+++ app/entities/user.py
@@ -19,6 +19,7 @@ class User(SQLModel, table=True):
     city: str
     country: str
     postal_code: str
+    
 
     bids: List["Bid"] = Relationship(back_populates="user")
     auctions: List["Auction"] = Relationship(back_populates="user")
